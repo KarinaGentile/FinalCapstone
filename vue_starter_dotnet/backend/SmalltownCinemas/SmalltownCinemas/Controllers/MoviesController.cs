9dffd1e05632f0873d33d82238670b89ddcb5737
Jacob Garner <jake.garner628@gmail.com>
2020-04-07 14:15:22 -0400

    fix merge conflicts

--- vue_starter_dotnet/backend/SmalltownCinemas/SmalltownCinemas/Controllers/MoviesController.cs
+++ vue_starter_dotnet/backend/SmalltownCinemas/SmalltownCinemas/Controllers/MoviesController.cs
@@ -18,30 +18,18 @@ namespace SmalltownCinemas.Controllers
         public MoviesController(IMovieDAO movieDAO)
         {
             this.movieDAO = movieDAO;
-<<<<<<< HEAD
-=======
-            
-        }
->>>>>>> b39f54f9f8d8b08e0f7aa6d039864f3dfd24ee2f
 
         }
+
+        
         [HttpGet]
         public IActionResult GetAllMovies()
         {
             return new JsonResult(movieDAO.GetAllMovies());
         }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 
-=======
-        [HttpGet("showings")]
-        public IActionResult GetAllShowings()
-        {
-            return new JsonResult(showingDAO.GetAllShowings());
-        }
->>>>>>> dd722565134a70ddf1a1fc19d45eb466879ddede
->>>>>>> b39f54f9f8d8b08e0f7aa6d039864f3dfd24ee2f
+        
+
     }
 }
