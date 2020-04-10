851b019be3073a2eb7a1833c4031991722ee6164
Jacob Garner <jake.garner628@gmail.com>
2020-04-10 15:04:43 -0400

    Started adding methods to ShowingSqlDAO to insert generated showings

--- vue_starter_dotnet/backend/SmalltownCinemas/SmalltownCinemas/DAL/ShowingSqlDAO.cs
+++ vue_starter_dotnet/backend/SmalltownCinemas/SmalltownCinemas/DAL/ShowingSqlDAO.cs
@@ -44,6 +44,41 @@ namespace SmalltownCinemas.DAL
             return showings;
         }
 
+        public void GenerateAllShowings()
+        {
+            List<Screen> screens = new List<Screen>();
+            screens.Add(new Screen(142, 0, 1, 1));
+            screens.Add(new Screen(142, 5, 1, 2));
+            screens.Add(new Screen(124, 0, 2, 3));
+            screens.Add(new Screen(124, 5, 2, 4));
+            screens.Add(new Screen(141, 0, 3, 5));
+            screens.Add(new Screen(141, 5, 3, 6));
+            screens.Add(new Screen(141, 10, 3, 7));
+            foreach (var screen in screens)
+            {
+                screen.AddShowtimes();
+            }
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.connectionString))
+                {
+                    conn.Open();
+
+                    string sql = "delete from showings";
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+
+                    //TODO: add insertion logic and looping
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
         private Showing RowToShowing(SqlDataReader rdr)
         {
             Showing showing = new Showing();
