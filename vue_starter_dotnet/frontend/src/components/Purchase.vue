b30302874cd46d291d7fb9c8e75c2c6b8055dee7
Karina Gentile <karinagentile381@gmail.com>
2020-04-13 15:33:05 -0400

    added purchase component

---
+++ vue_starter_dotnet/frontend/src/components/Purchase.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            streetAddress: "",
+            city: "",
+            state: "",
+            zipcode: "",
+            country: ""
+        
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
