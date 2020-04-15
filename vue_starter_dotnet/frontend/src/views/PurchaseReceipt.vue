2602cb1858b472c5ab6a86a91e9944d35e690a0f
Daniel James <businessjames1999@gmail.com>
2020-04-15 11:03:50 -0400

    added a model to purchaseReceipt.vue

--- vue_starter_dotnet/frontend/src/views/PurchaseReceipt.vue
+++ vue_starter_dotnet/frontend/src/views/PurchaseReceipt.vue
@@ -6,20 +6,28 @@
 
 <script>
 export default {
-name:"PurchaseReceipt",
-data() {
-  return {
-    purchaseId:0,
-    
+  name: "PurchaseReceipt",
+  data() {
+    return {
+      purchaseId: 0,
+      receipt: {
+        numTickets: 0,
+        customerName: "",
+        price: 0.0,
+        theatre: "",
+        startTime: "",
+        movieName: "",
+        seatNumbers: [],
+        purchaseTimeStamp: ""
+      }
+    };
+  },
+  created() {
+    this.purchaseId = this.$route.params.id;
+    //here we will look up the API to get the purchase with the Id
   }
-},
-created() {
-  this.purchaseId = this.$route.params.id
-  //here we will look up the API to get the purchase with the Id
-}
-}
+};
 </script>
 
 <style>
-
 </style>
