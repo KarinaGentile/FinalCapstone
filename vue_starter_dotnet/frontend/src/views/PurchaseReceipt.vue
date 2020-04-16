8ec5b93ce0a3c0860b996844bec261b5047e0b6f
Daniel James <businessjames1999@gmail.com>
2020-04-16 10:12:01 -0400

    added CSS code to the receipt page

--- vue_starter_dotnet/frontend/src/views/PurchaseReceipt.vue
+++ vue_starter_dotnet/frontend/src/views/PurchaseReceipt.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
-    <section class="centeredPanel">
+    <section id="centeredPanel">
+      <div id="info">
       <h2>Receipt for Purchase #: {{purchaseId}}</h2>
+      <p>**********************************************************</p>
+      <p id="bold">Movie title: {{receipt.movieName}}</p>
+      <p>**********************************************************</p>
       <p>Number of tickets: {{receipt.numTickets}}</p>
       <p>Customer name: {{receipt.customerName}}</p>
-      <p>Total price: {{receipt.price}}</p>
+      
       <p>Theater number: {{receipt.theater}}</p>
       <p>Start time: {{receipt.startTime}}</p>
-      <p>Movie title: {{receipt.movieName}}</p>
+      
+      <p>**********************************************************</p>
+      <p id="bold">Total price: ${{receipt.price}}</p>
+      <p>**********************************************************</p>
+
+      </div>
+      <div id="info2" >
       <ul>
-        <p>Seat numbers</p>
-        <li v-bind:key="sn" v-for="sn in receipt.seatNumbers">{{sn}}</li>
+      <p>Seat numbers</p>
+      <li v-bind:key="sn" v-for="sn in receipt.seatNumbers">{{sn}}</li>
       </ul>
-      <p>Purchase timestamp: {{receipt.purchaseTimeStamp}}</p>
+      <p id="purchase">Purchase timestamp: {{receipt.purchaseTimeStamp}}</p>
+      </div>
     </section>
   </div>
 </template>
@@ -47,7 +58,41 @@ export default {
 </script>
 
 <style>
-.centeredPanel{
-  position: relative;
+#centeredPanel{
+  clear: both;
+  display: block;
+  width: 100%;
+  min-height: 450px;
+  max-width: 750px;
+  background-color: white;
+  border-color: black;
+  border-style: solid;
+
+  border-radius: 7px;
+  margin: 20px auto;
+  padding-top: 0.5px;
+  padding-bottom: 50px;
+  padding-right: 20px;
+  padding-left: 20px;
+  align-content: center;
+  text-align: center;
 }
+#info{
+  text-align: left;
+  
+}
+#info2{
+  padding-top: 5px;
+  text-align: center;
+}
+#purchase{
+  padding-top: 5%;
+}
+
+#bold{
+  font-weight: bold;
+  text-align: center;
+}
+
+
 </style>
