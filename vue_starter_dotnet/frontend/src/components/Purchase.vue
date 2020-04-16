341f74b2e37d438dbe56e94d86f67230037cac1d
Rachel Huch <rachelhuch@gmail.com>
2020-04-16 10:05:04 -0400

    billingupdate

--- vue_starter_dotnet/frontend/src/components/Purchase.vue
+++ vue_starter_dotnet/frontend/src/components/Purchase.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
     <section class="centeredPanel">
-      <h3>Billing Information</h3>
+      <h3 class="text">Billing Information</h3>
       <p></p>
       <form>
         <ul class="flexcontainer">
-          <label>First Name:</label>
+          <label class="text">First Name:</label>
           <input id="firstName" type="text" required v-model="firstName" />
-          <label>Last Name:</label>
+          <label class="text">Last Name:</label>
           <input id="lastName" type="text" required v-model="lastName" />
-          <label>Email:</label>
+          <label class="text">Email:</label>
           <input id="email" type="text" required v-model="email" />
-          <label>Street Address:</label>
+          <label class="text">Street Address:</label>
           <input id="streetAddress" type="text" required="true" />
-          <label>City:</label>
+          <label class="text">City:</label>
           <input id="city" type="text" required="true" />
-          <label>State:</label>
+          <label class="text">State:</label>
           <input id="state" type="text" required="true" v-model="state" />
-          <label>Zip Code:</label>
+          <label class="text">Zip Code:</label>
           <input id="zipcode" type="text" required="true" v-model="zipcode" />
-          <label>Country:</label>
+          <label class="text">Country:</label>
           <input id="country" type="text" required="true" v-model="country" />
         </ul>
         <input @click="buyTickets" class="block" type="button" value="Buy Tickets" />
@@ -73,7 +73,16 @@ export default {
     showtime: String
   }
 };
+
+
 </script>
 
 <style>
+.text{
+  color:white;
+  text-align: center;
+}
+.block{
+  align-content: center;
+}
 </style>
